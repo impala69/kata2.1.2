56b7a75c43dd99a3184db273df8f3f2fdfc4ece7
max <sharg97@gmail.com>
2022-01-12 02:56:10 +0300

    sdelano1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanWorld =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanWorld.getMessage());
+        HelloWorld beanWorld2=
+                (HelloWorld)applicationContext.getBean("helloworld");
+        System.out.println(beanWorld2.getMessage());
+
+        Cat beanCat=(Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat2=(Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+
+        System.out.println(beanWorld==beanWorld2);
+        System.out.println(beanCat==beanCat2);
     }
 }
